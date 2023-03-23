4d8b83ac7b3e9da7a7b118cf5d36d6952bbb076e
LoTuS-SK <109459024+LoTuS-SK@users.noreply.github.com>
2023-03-23 20:56:37 +0300

    test1

--- src/componens/menu/Menu.jsx
+++ src/componens/menu/Menu.jsx
@@ -4,19 +4,43 @@ import QRCode from "react-qr-code";
 // import Burger from '../burger/Burger';
 import Snaks from './snaks/snaks';
 import "../menu/menu.css"
+import { snakes } from "../../data/database"
 
 
 
 const Menu = () => {
 
+const [parser_list,set_parser_list]=useState([])
 const [chek_list,set_chek_list]=useState([])
 const[ display_snakes, set_display_snakes ]= useState(false)
 const [display_menu,set_display_menu] = useState(true)
 
 
+
 const menu_eat = ["Закуски","Салаты","Горячие блюда","Супы","Гриль","Аквариум","Печь","Гарниры","Напитки"]
 
 
+function parser1(){
+  
+  let list = []
+for( let elem of snakes){
+
+  for(let elem2 of chek_list){
+      if (elem.name === elem2.name){
+         let obj = {id:elem.id,count:elem2.count}
+        list.push(obj)
+      } 
+      
+      }
+      
+  }
+  return list
+} 
+
+function test(){
+  set_parser_list([...parser1()])
+
+}
 
 
 const click =(e)=>{
@@ -53,22 +77,28 @@ if (chek_list.length>=1){
 }
 }
 
-function back_snakes(){
+function back_snakes(){ 
   set_display_menu(true)
   set_display_snakes(false)
 }
 
-function chek(){
-  console.log(query)
-}
+// function chek(){
+//   console.log(query)
+// }
 
-const query = `https://teal-lamington-08734d.netlify.app/salat?name=${JSON.stringify(chek_list)}`
-//const query = `http://localhost:3001/salat?name=${JSON.stringify(chek_list)}`
+const query = `https://teal-lamington-08734d.netlify.app/salat?name=${JSON.stringify(parser_list)}`
+//const query = `http://localhost:3001/salat?name=${JSON.stringify(parser_list)}`
+
+ 
+
+
+
 
 
   return (
     <>
-  
+   
+    <button onClick={test}>test</button>
     <Snaks display={display_snakes} func={add} back={back_snakes}/>
     
     <div className={display_menu?"wrap":"wrap none"}>
@@ -80,7 +110,7 @@ const query = `https://teal-lamington-08734d.netlify.app/salat?name=${JSON.strin
         {menu_eat.map((el)=>(<div className='menu-items' onClick={click}>{el}</div>))}
         </div>   
           
-    <div className='qrcode-wrap' onClick={chek}>
+    <div className='qrcode-wrap'>
          <Link to={query}>
                 <QRCode
                     size={256}
--- src/componens/salats/Salats.jsx
+++ src/componens/salats/Salats.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSearchParams } from "react-router-dom"
- 
+import { snakes } from '../../data/database' 
 
 const Salats = () => {
 
@@ -15,11 +15,25 @@ const Salats = () => {
  const str = decodeURI(queryParameters.get("name"))
 
   const arr = JSON.parse(str)
+
+  function pars_url(){
+    let list = []
+    for(let elem of snakes){
+      for(let elem2 of arr){
+        if(elem2.id === elem.id){
+          list.push(elem)
+        }
+      }
+    }
+    return list
+  }
+
+
   
   return (
     <div>
       <h1>Заказ</h1>
-      {arr.map((el=>(
+      {pars_url().map((el=>(
         <div>{el.name}</div>
       )))}
       <div>{str}</div>
