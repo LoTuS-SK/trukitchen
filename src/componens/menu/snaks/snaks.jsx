4fb02dc3f2e019d1e3504e47cd04f3302c66d4bf
LoTuS-SK <109459024+LoTuS-SK@users.noreply.github.com>
2023-06-24 13:58:26 +0300

    vers01

--- src/componens/menu/snaks/snaks.jsx
+++ src/componens/menu/snaks/snaks.jsx
@@ -4,6 +4,8 @@ import strelka from "../../../png/strelka.png"
 import "../snaks/snaks.css"
 import { useSelector} from 'react-redux'
 
+import pashtet from "../../../png/foot/pashtet.png"
+
 const Snaks = ({display,func,back}) => {
 
 const lean = useSelector(state=>state.one)
@@ -15,6 +17,18 @@ const lean = useSelector(state=>state.one)
     <div className={display?"skakes":"snakes none"}>
     <div className='strelka' onClick={back}><img src={strelka} alt="" /></div>
     <div className='snakes-item'>{lean==="ru"?"Закуски":"Snacks"}</div>
+    
+    
+    <div className='item_pal' tabIndex="0" >
+            <div className='label' onClick={func}>{lean==="ru"?"Паштет из оленины с румяными тостами и брусничным джемом":"Venison pate with ruddy toast and lingonberry jam"}</div>
+            <div className="wr_img"><img src={pashtet} alt="" /></div>
+            <span className='price-pl'>450 ₽</span>
+          </div>
+
+    
+    
+    
+    
     {lean==="ru" && snakes.map(el=>(
         <div className='item' tabIndex="0" onClick={func} key={el.id}><span className='label'>{el.name}</span><span className='price'>{el.price} ₽</span></div>
     ))}
