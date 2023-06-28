5ffea7d29ff689c9701d2b1362660021df9dcbe2
LoTuS-SK <109459024+LoTuS-SK@users.noreply.github.com>
2023-06-28 19:28:24 +0300

    rezerv

--- src/componens/bron/bron.jsx
+++ src/componens/bron/bron.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+
+import React ,{useState} from 'react'
 import logo1 from "../../png/bron/logo1.png"
 import logo2 from "../../png/bron/logo2.png"
 import "./bron.css"
 
 const Bron = () => {
+
+const [name,setname]= useState("")
+const [tel,settel]=useState("")
+
+function setnames(e){
+    setname(e.target.value)
+}
+
+function submit(){
+    
+    console.log(name)
+}
+
   return (
     <>
     <div className="bron_wr">
@@ -12,12 +26,15 @@ const Bron = () => {
         </div>
         <div className="wr_content">
             <h2 className='bron_h2'>Бронирование столика</h2>
-            <input type="text" name="name" placeholder='Ваше имя'/>
-            <input type="tel" name="tel" placeholder="номер телефона"/>
+            <input type="text" name="name" placeholder='Ваше имя' value={name} onChange={setnames}/>
+            <input type="tel" name="tel" placeholder="номер телефона" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/>
             <input type="numder" placeholder="Количетво персон"/>
-            <input type="date" name="" id="" className='data' placeholder="Дата"/>
+                <div className="data_wr">
+                    <label for="date">Дата</label>
+                    <input type="date" name="" id="date" className='data'/>
+                </div>
                 <div className="wr_button">
-                    <div className="button">Отправить</div>
+                    <div className="button" onClick={submit}>Отправить</div>
                 </div>
         </div>
         <div className="img_footer">
